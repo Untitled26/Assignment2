45d2ac4a985a993ec110373b70902308cb9e25fe
Untitled26 <82178586+Untitled26@users.noreply.github.com>
2024-02-23 23:56:05 -0500

    Add files via upload

---
+++ Assignment Story.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MASTER CHEF HAS JOINED THE SERVER. \n\nCyberpunk 2077. Skyscrapers dominate the city skyline. \n\nOn the top floors of Arisaka: \nrain beats on the window like tiny drums. Hundreds of players, Chromed out, barricade the elevator doors and staircase entrances; putting up tables as shields; and grip their guns- staring at the elevator doors ready for the Highrider from another world. \nSuddenly a loud crash interrupts the beat of the rain. Window Glass shards fly everywhere. A Satara Shotgun flies through the air as gun shots echo the scene. Master Chef flies into the scene instantly flatlining dozens of players, fly kicking into Keanu. \n\n'John Wick this kick'. \n\nA Monty Oum esk fighting style begins to errupt the scene. \n\n\nFrom the streets below we see a pair of boots step into the puddles of rain. David Martinez and Maine enter the building. Hundreds of Netrunners scrape the Net for way to take down the beast in Navy Green armor. \n\nHundreds players slaughtered by the second. A body is thrown out the window flying by David and Maine as they run up the stairs. \nBursting into the scene through the Barricaded doors, Maine manages to pin down Master Chef's arm as David makes a play for his head. 'John, Eliminate the Boy first. The Brute can be dealt with while there's less noise'. BANG. David falls. His Cyber Skeleton is ripped out of him. Maine stare's at Master Chef... Soon to meet his demise. Miles away, a Netrunner find a way to take down Master Chef... Cortana. 'Maybe we can save the server'");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
